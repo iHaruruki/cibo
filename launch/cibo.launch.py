f7b699bfd0e64c361e5140451d2e045bdf7364b5
HarukiIsono <haruki.isono861@gmail.com>
2025-10-05 20:47:10 +0900

    fix: launch file

--- launch/cibo.launch.py
+++ launch/cibo.launch.py
@@ -3,6 +3,16 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='cibo', executable='holistic_camera_01_node', name='holistic_camera_01'),
-        Node(package='cibo', executable='holistic_camera_02_node', name='holistic_camera_02'),
+        Node(
+            package='cibo',
+            executable='front_camera_node',
+            name='front_camera_node',
+            output='screen'
+        ),
+        Node(
+            package='cibo',
+            executable='top_camera_node',
+            name='top_camera_node',
+            output='screen'
+        ),
     ])
