4c8f57c56e43d43993e96ae32b6d532af6b038ad
HarukiIsono <haruki.isono861@gmail.com>
2025-10-12 23:42:38 +0900

    fix

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -1,12 +1,20 @@
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
 
 def generate_launch_description():
-    node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=[
-            '-d', './rviz/cibo_camera.rviz'])
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('cibo'),
+        'rviz', 'cibo_camera.rviz'
+    )
 
-    return LaunchDescription([node])
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            output='screen'
+        ),
+    ])
