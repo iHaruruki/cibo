64858348fc68ae0c4f3c6b2081fa03b895c2d435
robot <robot@syblab.org>
2025-10-14 23:28:34 +0900

    fix

--- cibo/top_camera_depth.py
+++ cibo/top_camera_depth.py
@@ -40,7 +40,7 @@ class TopCameraNode(Node):
 
         # === Topic / frame params ===
         self.color_topic = self.declare_parameter('color_topic', '/camera_02/color/image_raw').value
-        self.depth_topic = self.declare_parameter('depth_topic', '/camera_02/depth/image_rect_raw').value
+        self.depth_topic = self.declare_parameter('depth_topic', '/camera_02/depth/image_raw').value
         self.depth_info_topic = self.declare_parameter('depth_info_topic', '/camera_02/depth/camera_info').value
         self.camera_frame = self.declare_parameter('camera_frame', 'camera_02_depth_optical_frame').value
         self.tf_rate_hz = float(self.declare_parameter('tf_rate_hz', 20.0).value)
