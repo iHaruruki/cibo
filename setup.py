2dc5cf183f3a728509435971f4e748d8fc1aad84
robot <robot@syblab.org>
2025-10-15 19:44:53 +0900

    add: robot_state_publisher

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
         (os.path.join('share', package_name, 'rviz'),   glob('rviz/*.rviz')),
-        (os.path.join('share', package_name, 'urdf'),   glob('urdf/*.urdf')),
+        (os.path.join('share', package_name), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
