47673846df4fd70634a06bf4525b4f7b2efa5e2c
HarukiIsono <haruki.isono861@gmail.com>
2025-10-21 17:35:13 +0900

    fix

--- launch/cibo_depth.launch.py
+++ launch/cibo_depth.launch.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2025 Haruki Isono
-# This software is released under the MIT License, see LICENSE.
+# Copyright (c) 2025
+# MIT License
 
 import os
 from ament_index_python.packages import get_package_share_directory
@@ -10,24 +10,21 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
     urdf_file_name = 'cibo.urdf'
-
-    urdf = os.path.join(
-        get_package_share_directory('cibo'),
-        urdf_file_name)
-
-    # Open and read the URDF file
-    with open(urdf, 'r') as infp:
-        robot_desc = infp.read()
+    urdf = os.path.join(get_package_share_directory('cibo'), urdf_file_name)
+    with open(urdf, 'r') as f:
+        robot_desc = f.read()
 
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+            description='Use simulation (Gazebo) clock if true'
+        ),
+
+        # robot_state_publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -36,16 +33,39 @@ def generate_launch_description():
             parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
             arguments=[urdf]
         ),
+
+        # Dual camera sync republisher
+        Node(
+            package='cibo',
+            executable='dual_camera_sync_node',
+            name='dual_camera_sync_node',
+            output='screen',
+            parameters=[{'queue_size': 20, 'slop_sec': 0.08, 'restamp': False}],
+        ),
+
+        # front_camera_depth_node -> /sync/camera_01/** を購読
         Node(
             package='cibo',
             executable='front_camera_depth_node',
             name='front_camera_node',
-            output='screen'
+            output='screen',
+            remappings=[
+                ('/camera_01/color/image_raw', '/sync/camera_01/color/image_raw'),
+                ('/camera_01/depth/image_raw', '/sync/camera_01/depth/image_raw'),
+                ('/camera_01/depth/camera_info', '/sync/camera_01/depth/camera_info'),
+            ]
         ),
+
+        # top_camera_depth_node -> /sync/camera_02/** を購読
         Node(
             package='cibo',
             executable='top_camera_depth_node',
             name='top_camera_node',
-            output='screen'
+            output='screen',
+            remappings=[
+                ('/camera_02/color/image_raw', '/sync/camera_02/color/image_raw'),
+                ('/camera_02/depth/image_raw', '/sync/camera_02/depth/image_raw'),
+                ('/camera_02/depth/camera_info', '/sync/camera_02/depth/camera_info'),
+            ]
         ),
     ])
