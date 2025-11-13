e08658844be7515c36f52e3f868d15d431fb7cf4
robot <robot@syblab.org>
2025-11-13 14:27:35 +0900

    fix: cibo_depth.launch.py

--- launch/cibo_depth.launch.py
+++ launch/cibo_depth.launch.py
@@ -35,13 +35,13 @@ def generate_launch_description():
         ),
 
         # Dual camera sync republisher
-        Node(
-            package='cibo',
-            executable='dual_camera_sync_node',
-            name='dual_camera_sync_node',
-            output='screen',
-            parameters=[{'queue_size': 20, 'slop_sec': 0.08, 'restamp': False}],
-        ),
+        # Node(
+        #     package='cibo',
+        #     executable='dual_camera_sync_node',
+        #     name='dual_camera_sync_node',
+        #     output='screen',
+        #     parameters=[{'queue_size': 20, 'slop_sec': 0.08, 'restamp': False}],
+        # ),
 
         # front_camera_depth_node -> /sync/camera_01/** を購読
         Node(
@@ -49,11 +49,11 @@ def generate_launch_description():
             executable='front_camera_depth_node',
             name='front_camera_node',
             output='screen',
-            remappings=[
-                ('/camera_01/color/image_raw', '/sync/camera_01/color/image_raw'),
-                ('/camera_01/depth/image_raw', '/sync/camera_01/depth/image_raw'),
-                ('/camera_01/depth/camera_info', '/sync/camera_01/depth/camera_info'),
-            ]
+            # remappings=[
+            #     ('/camera_01/color/image_raw', '/sync/camera_01/color/image_raw'),
+            #     ('/camera_01/depth/image_raw', '/sync/camera_01/depth/image_raw'),
+            #     ('/camera_01/depth/camera_info', '/sync/camera_01/depth/camera_info'),
+            # ]
         ),
 
         # top_camera_depth_node -> /sync/camera_02/** を購読
@@ -62,10 +62,10 @@ def generate_launch_description():
             executable='top_camera_depth_node',
             name='top_camera_node',
             output='screen',
-            remappings=[
-                ('/camera_02/color/image_raw', '/sync/camera_02/color/image_raw'),
-                ('/camera_02/depth/image_raw', '/sync/camera_02/depth/image_raw'),
-                ('/camera_02/depth/camera_info', '/sync/camera_02/depth/camera_info'),
-            ]
+            # remappings=[
+            #     ('/camera_02/color/image_raw', '/sync/camera_02/color/image_raw'),
+            #     ('/camera_02/depth/image_raw', '/sync/camera_02/depth/image_raw'),
+            #     ('/camera_02/depth/camera_info', '/sync/camera_02/depth/camera_info'),
+            # ]
         ),
     ])
